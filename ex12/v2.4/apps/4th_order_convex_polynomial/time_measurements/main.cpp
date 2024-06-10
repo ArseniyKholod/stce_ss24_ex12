ab5ca01f735ab297491fadacfd1c4ee7e7774f4e
stce <stce@stce.stce.rwth-aachen.de>
2024-06-10 17:23:01 +0200

    change write out in examples code a bit

--- ex12/v2.4/apps/4th_order_convex_polynomial/time_measurements/main.cpp
+++ ex12/v2.4/apps/4th_order_convex_polynomial/time_measurements/main.cpp
@@ -7,14 +7,13 @@
 #include <iostream>
 #include <fstream>
 #include <chrono>
-#include <vector>
 
 int main() {
   try{
     using T=double;
     using namespace std;
     int n_start=1, n_stop=81, n_step=4;
-    std::vector<T> time_measurements;
+    std::ofstream out("time.plt");
     for(int n=n_start; n<=n_stop; n+=n_step){
       int np=n, nx=np; 
       la::vector_t<T> p=2*la::vector_t<T>::Ones(np); 
@@ -24,11 +23,8 @@ int main() {
       for(int i=0; i<5; i++)
         minimizer.run(x,p);
       auto stop = std::chrono::high_resolution_clock::now();
-      time_measurements.push_back(std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count()/5.*0.000001);
+      out<<nx<<' '<<(std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count()/5.*0.000001)<<std::endl;
     }
-    std::ofstream out("time.plt");
-    for(int i=0; n_start+i*n_step<=n_stop; i++)
-      out<<n_start+i*n_step<<' '<<time_measurements[i]<<std::endl;
   }
   catch(ex::cppNum_exception & e){
     std::cerr<<e.what()<<std::endl;
--- v2.4/apps/cosh/time_measurements/main.cpp
+++ v2.4/apps/cosh/time_measurements/main.cpp
@@ -6,14 +6,13 @@
 #include <iostream>
 #include <fstream>
 #include <chrono>
-#include <vector>
 
 int main() {
   using T=double;
   using solver=la::lu_solver_t<T>;
   using namespace std;
   int n_start=1, n_stop=101, n_step=4;
-  std::vector<T> time_measurements;
+  std::ofstream out("time.plt");
   for(int n=n_start; n<=n_stop; n+=n_step){
     int np=n, nx=np; 
     la::vector_t<T> p=2*la::vector_t<T>::Ones(np); 
@@ -23,10 +22,7 @@ int main() {
     for(int i=0; i<5; i++)
       minimizer.run(x,p);
     auto stop = std::chrono::high_resolution_clock::now();
-    time_measurements.push_back(std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count()/5.*0.000001);
+    out<<nx<<' '<<(std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count()/5.*0.000001)<<std::endl;
   }
-  std::ofstream out("time.plt");
-  for(int i=0; n_start+i*n_step<=n_stop; i++)
-    out<<n_start+i*n_step<<' '<<time_measurements[i]<<std::endl;
   return 0;
 }  
